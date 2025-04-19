11d85bc8ad383dcfd92c2c954e06243860241b4d
Suyash Kumar Singh <blogbydev@gmail.com>
2025-04-19 13:40:01 +0530

    download these files

---
+++ download.sh
@@ -0,0 +1,4 @@
+https://cdn.upgrad.com/uploads/production/9a461c3b-8a69-4d63-985c-5dfa4661cde7/HelpMateAI_RAG_Files.zip
+mkdir -p ./downloads
+curl -o ./downloads/HelpMateAI_RAG_Files.zip https://cdn.upgrad.com/uploads/production/9a461c3b-8a69-4d63-985c-5dfa4661cde7/HelpMateAI_RAG_Files.zip
+
